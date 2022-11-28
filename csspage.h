6676f7cbbc2a64ad4bba744211e6046b4e3f6b77
arihantjain-01 <119114324+arihantjain-01@users.noreply.github.com>
2022-11-28 15:59:06 +0530

    Create csspage.h

---
+++ csspage.h
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fpage.css">
+    <title>HTML TUTORIALS </title>
+</head>
+<body>
+ <div class="navbar" id="nav">
+ <a href="#" style="text-decoration:none ;"><span id="brand" style="font-family:Candara;">&nbsp;<em>#Code</em>&nbsp;</span>
+<span id="brand1" ><U>Shiksha</U></span></a>
+ </div>
+<div>
+<span id="htmlhead"> HTML TUTORIALS</span>
+<OL id="htmllink" style="padding-top:15px";>
+<li><a href="#">CSS EDITORS</a></li>
+<li><a href="#">CSS BASICS</a></li>
+<li><a href="#">CSS ELEMENTS</a></li>
+<li><a href="#">CSS ATTRIBUTES</a></li>
+<li><a href="#">CSS HEADINGS</a></li>
+<li><a href="#">CSS PARAGRAPHS</a></li>
+<li><a href="#">CSS STYLES</a></li>
+<li><a href="#">CSS FORMATTING</a></li>
+<li><a href="#">CSS QUOTATIONS</a></li>
+<li><a href="#"> CSS COMMENTS</a></li>
+<li><a href="#">CSS COLORS</a></li>
+<li><a href="#">CSS LINKS</a></li>
+<li><a href="#">CSS IMAGES</a></li>
+<li><a href="#">CSS TABLES</a></li>
+<li><a href="#">CSS LISTS</a></li>
+
+</OL>
+</div>
+
+
+
+</body>
+</HTML>
